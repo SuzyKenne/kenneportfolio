268020dca7e5430a0d646717cbe4b6b2fa119a0b
SuzyKenne <you@example.com>
2023-05-16 08:17:37 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+export const App = () => {
+     return ( <div>
+         App 
+         </div> ) }
+
+
+import './index.css';
+import Header from './components/header/Header'
+ import Navbar from './components/navbar/Navbar'
+  import About from './components/about/About' 
+  
+   import Services from './components/services/Services' 
+   import Portfolio from './components/portfolio/Portfolio' 
+   import Testimonial from './components/testimonial/Testimonial'
+    import Contact from './components/contact/Contact' 
+    import Footer from './components/footer/Footer'
+
+function App() {
+     return ( 
+     <> 
+     <Header/>
+      <Navbar/> 
+      <About/> 
+     
+       <Services/>
+        <Portfolio/>
+        <Testimonial/> 
+<Contact/>
+ <Footer/> 
+ </>
+);
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ import ReactDOM from 'react-dom/client';
+ import './index.css';
+ import APP from './App';
+
+ const root = ReactDOM.createRoot(document.getElementsById('root'));
+
+
+  root.render(
+  <React.StrictMode>
+    <APP/> 
+    </React.StrictMode>
+        ) 
+        export default APP ;
